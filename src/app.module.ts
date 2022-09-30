fc9acc311baaa9f00a32334bf6dfb44dbaae4761
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-30 11:50:06 +0300

    Add models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
+import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ProductModule, ReviewModule, TopPageModule],
   controllers: [AppController],
   providers: [AppService],
 })

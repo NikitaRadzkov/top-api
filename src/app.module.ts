ab8dfd9bc67c394889de7fc3afc5062ea3a271f5
Nikita Radzkou <nikita.radzkou@gmail.com>
2022-12-11 21:45:10 +0300

    Init MongoDB to API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { getMongoConfig } from './configs/mongo.config';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
-  imports: [AuthModule, ProductModule, ReviewModule, TopPageModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
+    AuthModule,
+    ProductModule,
+    ReviewModule,
+    TopPageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

412d4a4b45b483822d62f85c3cbb2ab4b1d162f9
Nikita Radzkou <nikita.radzkou@gmail.com>
2022-12-17 00:35:11 +0300

    Add validation pipe

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,8 +1,22 @@
+import { IsNumber, IsString, Max, Min } from 'class-validator';
 export class CreateReviewDto {
+  @IsString()
   name: string;
+
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  @Max(5)
+  @Min(1, { message: 'Rating cannot be less 1' })
+  @IsNumber()
   rating: number;
+
+  @IsString()
   productId: string;
+
+  @IsString()
   typegooseName: string;
 }
